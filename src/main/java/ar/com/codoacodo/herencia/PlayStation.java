f0e2939edba5aabe5f7be6f47a1f4fb6352804bb
carlos horacio lopez <krloss@gmail.com>
2022-11-23 19:41:43 -0300

    clase23-11
    herencia

---
+++ src/main/java/ar/com/codoacodo/herencia/PlayStation.java
@@ -0,0 +1,16 @@
+package ar.com.codoacodo.herencia;
+
+public class PlayStation extends Consola {
+
+	private int version;
+	
+	public PlayStation(int version) {
+		//invocar al constructor del padre
+		super(1024,"PlayStation");
+		this.version = version;
+	}
+	
+	public int getVersion() {
+		return this.version;
+	}
+}
--- src/main/java/ar/com/codoacodo/interfaces/MainInterfaces.java
+++ src/main/java/ar/com/codoacodo/interfaces/MainInterfaces.java
@@ -1,6 +1,7 @@
 package ar.com.codoacodo.interfaces;
 
 import ar.com.codoacodo.idiomas.IHablar;
+import ar.com.codoacodo.idiomas.IIdioma;
 import ar.com.codoacodo.idiomas.Perro;
 import ar.com.codoacodo.idiomas.Persona;
 
@@ -11,8 +12,8 @@ public class MainInterfaces {
 		// e invoco el hablar de cada una
 		
 		//Clase nombreObjeto = new Clase();
-		
-		Persona p = new Persona("pilar", "ingles");		
+		IIdioma ingles = new Ingles();
+		Persona p = new Persona("pilar", ingles);		
 		Perro patan = new Perro("patan");
 		
 		//por implementar IHablar las dos instancias
@@ -33,6 +34,6 @@ public class MainInterfaces {
 		i.hablar();
 		
 		Persona pDentroDei = (Persona)i;		
-		pDentroDei.decir("hola","ingles");//??
+		pDentroDei.decir("hola",ingles);//??
 	}
 }
